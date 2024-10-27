aa983691dc5bfe86097043bc7aca668b68277a09
MortalWombat-repo <69204832+MortalWombat-repo@users.noreply.github.com>
2024-10-27 17:43:57 +0100

    Delete homework/homework directory

--- homework/homework/q6_predict.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-url = "http://0.0.0.0:9696/predict"
-
-client = {"job": "management", "duration": 400, "poutcome": "success"}
-response = requests.post(url, json=client).json()
-
-print(response)
